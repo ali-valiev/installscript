6188549ea4bbae42ed1fa35f5f57a1b5f8657c02
ali-valiev <79750374+ali-valiev@users.noreply.github.com>
2024-06-08 13:46:57 +0400

    Update installscript-v2.sh

--- installscript-v2.sh
+++ installscript-v2.sh
@@ -22,13 +22,13 @@ type=1
 
 # Check the distribution type and install dependencies accordingly
 if [ "$type" -eq 1 ]; then
-    echo "Installing dependencies for Arch"
-    sudo pacman -S --needed git base-devel \
-			xorg libx11 libxft libxinerama freetype2 fontconfig \
-      feh neovim alacritty firefox curl wget unzip \
-			pipewire pipewire-pulse pavucontrol \
-			unclutter dunst openssh \
-			ttf-martian-mono-nerd otf-firamono-nerd
+  echo "Installing dependencies for Arch"
+  sudo pacman -S --needed git base-devel \
+    xorg libx11 libxft libxinerama freetype2 fontconfig \
+    feh neovim alacritty firefox curl wget unzip \
+    pipewire pipewire-pulse pavucontrol \
+    unclutter dunst openssh \
+    ttf-martian-mono-nerd otf-firamono-nerd
 elif [ "$type" -eq 2 ]; then
     echo "Debian is not supported for now"
     echo "Have to check package names..."
@@ -50,34 +50,34 @@ directories=(
 
 # Move directories to backup locations
 for dir in "${directories[@]}"; do
-    if [ -d "$dir" ]; then
-        mv "$dir" "${dir%/}-old/"
-    fi
+  if [ -d "$dir" ]; then
+    mv "$dir" "${dir%/}-old/"
+  fi
 done
 
 # Move .bashrc if it exists
 if [ -f "$home_dir/.bashrc" ]; then
-    mv "$home_dir/.bashrc" "$home_dir/.bashrc-old"
+  mv "$home_dir/.bashrc" "$home_dir/.bashrc-old"
 fi
 
 
 git clone "$github/dotfiles" "$dotfiles_dir"
-git clone "$github/nvim"		 "$config_dir/nvim/"
+git clone "$github/nvim"     "$config_dir/nvim/"
 git clone "$github/suckless" "$config_dir/suckless/"
 
 
-cp "$dotfiles_dir/bashrc"	"$home_dir/.bashrc"
+cp "$dotfiles_dir/bashrc"  "$home_dir/.bashrc"
 cp "$dotfiles_dir/xinitrc" "$home_dir/.xinitrc"
 
 cp -r "$dotfiles_dir/alacritty/" "$config_dir/alacritty/"
-cp -r "$dotfiles_dir/dunst/"		 "$config_dir/dunst/"
+cp -r "$dotfiles_dir/dunst/"	 "$config_dir/dunst/"
 
 cd "$config_dir/suckless/"
 echo "Sudo privileges are needed to compile and install DWM/Dmenu"
 bash build-all.sh
 
 if [ "$dwm_autostart" == "true"]; then
-	echo '
+  echo '
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     exec startx
 fi
